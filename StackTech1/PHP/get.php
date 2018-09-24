78db3c424b028ac5ac36b6de949f8fbbdc8914c5
UniBreakfast <unibreakfast1@gmail.com>
2018-09-24 19:13:58 +0300

    beFork

--- StackTech1/PHP/get.php
+++ StackTech1/PHP/get.php
@@ -3,9 +3,6 @@
   $query = "SELECT id, record, dt_create FROM test_list";
   $result = mysqli_query($db, $query) or exit ('SELECT records Query failed');
   $headerows = array('headers'=>array('record', 'dt_create'), 'rows'=>array());
-  while ($row = mysqli_fetch_row($result)) {
-//    $row[0] = (int)$row[0];
-    $headerows['rows'][] = $row;
-  }
+  while ($row = mysqli_fetch_row($result)) $headerows['rows'][] = $row;
   echo json_encode($headerows);
 ?>
