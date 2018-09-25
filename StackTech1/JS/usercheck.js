ff767193a54d9e274328fb2460a15e3c02d639c1
UniBreakfast <unibreakfast1@gmail.com>
2018-09-25 13:39:38 +0300

    Some thoughts in usercheck.js only.

--- StackTech1/JS/usercheck.js
+++ StackTech1/JS/usercheck.js
@@ -1,1 +1,17 @@
 /*
+Если есть cookie, отправить его в usercheck.php и получить ответ,
+если нету, считать, что ответ false.
+
+  Если ответ false:
+    Если страница не login или register то редирект на login.
+  иначе Если ответ true:
+    Если страница login или register то предложить выйти
+
+*/
+
+
+var inside = (location.pathname != "/StackTech1/login.htm" &&
+              location.pathname != "/StackTech1/register.htm");
+function act_on_check(response) {
+  if (response === 'valid' && inside)
+}
