8f8bcc3a3fba1c8bc822f338e57ff4ab986ae29b
unibreakfast <unibreakfast1@gmail.com>
2018-10-07 20:43:19 +0300

    Logout in php without JS

--- StackTech1/Auth/PHP/logout.php
+++ StackTech1/Auth/PHP/logout.php
@@ -2,15 +2,20 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'sandbox.php';
 require_once 'authvars.php';
 
-if (isset($_REQUEST['cookie']) && trim($_REQUEST['cookie'])!=='') {
+if (isset($_REQUEST['cookie']) && trim($_REQUEST['cookie'])!=='')
   $cookie = trim($_REQUEST['cookie']);
-  list($userid, $token) = explode('|', $cookie);
-
-  $query = "DELETE FROM $sessionTable
-            WHERE user_id = $userid AND token = '$token'";
-  mysqli_query($db, $query)
-    or exit ("DELETE FROM $sessionTable Query failed!");
+elseif (isset($_COOKIE['user'])) {
+  $cookie = $_COOKIE['user'];
+  setcookie('user', '', time()-1, '/');
 }
-else echo 'who do you want to log out?';
+else exit('no cookie found to log out user');
+
+list($userid, $token) = explode('|', $cookie);
+
+$query = "DELETE FROM $sessionTable
+          WHERE user_id = $userid AND token = '$token'";
+mysqli_query($db, $query)
+  or exit ("DELETE FROM $sessionTable Query failed!");
+header('Location: ../../login.htm');
 ?>
 
