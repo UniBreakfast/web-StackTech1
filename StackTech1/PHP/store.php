3001ebc963ce31ec486a9c81c78c1c40dfad4566
UniBreakfast <unibreakfast1@gmail.com>
2018-10-22 20:05:06 +0300

    WorkFlow master

--- StackTech1/PHP/store.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once $_SERVER['DOCUMENT_ROOT'].'sandbox.php';
-  if (isset($_REQUEST['record']) && trim($_REQUEST['record'])!=='') {
-    $record = trim($_REQUEST['record']);
-    $query = "INSERT test_list (record) VALUES ('$record')";
-    mysqli_query($db, $query) or exit ('INSERT record Query failed');
-  }
-  else echo "No record provided - none added";
-?>
