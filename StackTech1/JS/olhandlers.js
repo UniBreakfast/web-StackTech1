1fad1c5d6e8a1eaf0b3361692602a0b6d3d0e1bb
UniBreakfast <unibreakfast1@gmail.com>
2018-11-07 17:30:35 +0200

    Empty project for proof-of-concept type projects

--- StackTech1/JS/olhandlers.js
+++
@@ -1,6 +0,0 @@
-c.ol.onclick = e => {
-  if (e.target.className == 'remove') {
-    f.POST('PHP/remove.php?recid='+e.target.dataset.id);
-    c.btn_get.click();
-  }
-}
--- StackTech1/PHP/clear.php
+++
@@ -1,5 +0,0 @@
-<?php
-  require_once $_SERVER['DOCUMENT_ROOT'].'sandbox.php';
-  $query = 'DELETE FROM test_list';
-  mysqli_query($db, $query) or exit ('DELETE all FROM test_list Query failed');
-?>
