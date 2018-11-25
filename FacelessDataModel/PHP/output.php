c9ab593b4af6cc857d7eaaa11383cc74ce28d01b
UniBreakfast <unibreakfast1@gmail.com>
2018-11-25 18:28:24 +0200

    transport data from db

--- FacelessDataModel/PHP/output.php
+++ FacelessDataModel/PHP/output.php
@@ -1,6 +1,9 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'].'sandbox.php';
+require_once '../_Commons/PHP/krumo.php';
+require_once '../_Commons/PHP/f.php';
 
-
+/*
 $for_output = array (
   'endeavors' => array (
     'class' => 'Endeavor',
@@ -19,6 +22,20 @@ $for_output = array (
     )
   )
 );
+*/
+
+$test_endeavors =
+  f::getRecords($db, 'SELECT name, category, deadline FROM test_endeavors');
+
+$for_output = array (
+  'endeavors' => array (
+    'class'   => 'Endeavor',
+    'headers' => array ('name', 'category', 'deadline'),
+    'rows'    => $test_endeavors
+  )
+);
+//krumo($test_endeavors);
+//krumo($for_output);
 
 $output = json_encode($for_output);
 
