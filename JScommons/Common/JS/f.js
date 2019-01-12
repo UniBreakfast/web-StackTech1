2cb31bd5099bf24c8daf16c86bd602f8b12160c6
Mikhail Ninin aka "UniBreakfast <unibreakfast1@gmail.com>
2019-01-12 16:56:10 +0200

    xhr tweak

--- JScommons/Common/JS/f.js
+++ JScommons/Common/JS/f.js
@@ -7,34 +7,37 @@ const f = {
   newEl: document.createElement .bind(document),
 
   // to request do f.request(type, url, callback, reportcb, falldata, fallcb)
-  request: function request(type, url, cb, reportcb, falldata, fallcb, simonly) {
-  if (simonly) falldata ? fallcb(falldata) : fallcb();
-  else {
-    const xhr = new XMLHttpRequest();
-    xhr.open(type, url, true);
-    xhr.onload = () => {
-      if (fallcb) falldata ? fallcb(falldata) : fallcb();
-      else if (cb) {
-        if (xhr.status >= 200 && xhr.status < 400) {
-          if (!xhr.response.startsWith('<?php')) {
-            if (xhr.response !== '') cb(xhr.response);
-            else cb() || reportcb ? reportcb('php-response was empty') :0 ;
+  request: function request(type, url, cb, reportcb, falldata, fallcb, simonly, rawphp) {
+    if (simonly) falldata ? fallcb(falldata) : fallcb();
+    else {
+      const xhr = new XMLHttpRequest();
+      xhr.open(type, url, true);
+      xhr.onload = () => {
+        if (fallcb) falldata ? fallcb(falldata) : fallcb();
+        else if (cb) {
+          if (xhr.status >= 200 && xhr.status < 400) {
+            if (!xhr.response.startsWith('<?php') || rawphp) {
+              if (xhr.response !== '') cb(xhr.response);
+              else cb() || reportcb ? reportcb('php-response was empty') :0 ;
+            }
+            else if (reportcb && !rawphp) {
+              reportcb('php-file content returned instead of php-response');
+              falldata ? cb(falldata) : 0;
+            }
           }
-          else if (reportcb)
-            reportcb('php-file content returned instead of php-response');
-        }
-        else {
-          if (reportcb)
-            reportcb('unfortunately request.status is ' + xhr.status);
-          falldata ? cb(falldata) : cb();
+          else {
+            if (reportcb)
+              reportcb('unfortunately request.status is ' + xhr.status);
+            falldata ? cb(falldata) : cb();
+          }
         }
       }
+      xhr.onerror =
+        e => reportcb(type + ' request to '+ url + ' produced ' + e);
+      xhr.ontimeout =
+        () => reportcb(type + ' request to '+ url + ' timed out!');
+      xhr.send();
     }
-    xhr.onerror =
-      e => reportcb(type + ' request to '+ url + ' produced ' + e);
-    xhr.ontimeout =
-      () => reportcb(type + ' request to '+ url + ' timed out!');
-    xhr.send();
   }
 },
 
